4b72a370787b99b50aacc1935128eb767294c079
C.Hajduk <christian.hajduk@outlook.de>
2025-09-29 18:50:44 +0200

    add text-transform: capitalize

--- scripts/template.js
+++ scripts/template.js
@@ -1,8 +1,10 @@
 function generatePokemonTemplate(index) {
     return `
         <div class="pokemon-card">
-            <h2><span>#${allPkm[index].id}</span> ${allPkm[index].name}</h2>
-            <img src="./assets/logos/${allPkm[index].types[0].type.name}.jpg">
+            <div class="header-card-wrapper">
+                <h2><span>#${allPkm[index].id}</span> ${allPkm[index].name}</h2>
+                <img src="./assets/logos/${allPkm[index].types[0].type.name}.jpg">
+            </div>
             <div class="pokemon-image ${allPkm[index].types[0].type.name}" >
                 <img src="${allPkm[index].sprites.other.showdown.front_default}" loading="lazy">
             </div>
