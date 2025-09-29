f467e82c2d805db3de292b4361a28b1fa6b5c0dc
C.Hajduk <christian.hajduk@outlook.de>
2025-09-29 18:10:04 +0200

    add typ backgroundcolor

--- scripts/template.js
+++ scripts/template.js
@@ -2,8 +2,8 @@ function generatePokemonTemplate(index) {
     return `
         <div class="pokemon-card">
             <h2><span>#${allPkm[index].id}</span> ${allPkm[index].name}</h2>
-            <img src="./assets/logos/${allPkm[index].types[0].type.name}.jpg">          
-            <div class="pokemon-image">
+            <img src="./assets/logos/${allPkm[index].types[0].type.name}.jpg">
+            <div class="pokemon-image ${allPkm[index].types[0].type.name}" >
                 <img src="${allPkm[index].sprites.other.showdown.front_default}" loading="lazy">
             </div>
         </div>
